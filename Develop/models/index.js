07daf507fdd8b8b2c59822cb69186177e5633f6d
tjansson-ui <59398067+tjansson-ui@users.noreply.github.com>
2023-09-05 17:45:45 -0500

    added initial model types

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: ""
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ""
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: ""
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: ""
+})
 
 module.exports = {
   Product,
